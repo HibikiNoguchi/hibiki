34d6c2ea2e700f308e73b69c4ec70aebd4ab6a39
Hibiki Noguchi <hibiki4274@gmail.com>
2022-01-07 16:01:07 +0900

    routeの変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'home#index'
-  get 'home/homepage' => 'home#homepage'
+
   resources :instagrams do
     resources :likes, only: [:create, :destroy]
     resources :comments, only: [:create, :destroy] do
       resources :comment_likes, only: [:create, :destroy]
     end
   end
+  
   resources :relationships, only: [:create, :destroy]
   resources :messages, :only => [:create]
   resources :rooms, :only => [:create, :show, :index]
+  root 'instagrams#index'
+  
 
 end
